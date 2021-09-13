5a488cde74580bba89e08d20b90bca1199ef3bb1
Yongli Chen <yonglichen@edgenesis.com>
2021-09-13 09:46:27 +0000

    http server

--- deviceshifu/pkg/deviceshifu/deviceshifu.go
+++ deviceshifu/pkg/deviceshifu/deviceshifu.go
@@ -2,14 +2,28 @@ package deviceshifu
 
 import (
 	"fmt"
+	"net/http"
 )
 
 type DeviceShifu struct {
 	Name string
 }
 
-func (ds *DeviceShifu) Start() error {
-	fmt.Println(ds.Name)
+func (ds *DeviceShifu) deviceShifuHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, ds.Name)
+}
+
+func (ds *DeviceShifu) startHttpServer(stopCh <-chan struct{}) error {
+	http.HandleFunc("/", ds.deviceShifuHandler)
+	http.ListenAndServe(":8000", nil)
+
+	return nil
+}
+
+func (ds *DeviceShifu) Start(stopCh <-chan struct{}) error {
+	fmt.Printf("deviceShifu %s started\n", ds.Name)
+
+	go ds.startHttpServer(stopCh)
 
 	return nil
 }
--- deviceshifu/pkg/deviceshifu/deviceshifu_test.go
+++ deviceshifu/pkg/deviceshifu/deviceshifu_test.go
@@ -1,7 +1,11 @@
 package deviceshifu
 
 import (
+	"net/http"
 	"testing"
+	"time"
+
+	"k8s.io/apimachinery/pkg/util/wait"
 )
 
 func TestStart(t *testing.T) {
@@ -9,7 +13,21 @@ func TestStart(t *testing.T) {
 		Name: "test",
 	}
 
-	if err := mockds.Start(); err != nil {
+	if err := mockds.Start(wait.NeverStop); err != nil {
 		t.Errorf("DeviceShifu.Start failed due to: %v", err.Error())
+	}
+}
+func TestStartHttpServer(t *testing.T) {
+	mockds := &DeviceShifu{
+		Name: "test",
+	}
+
+	go mockds.startHttpServer(wait.NeverStop)
+
+	time.Sleep(1 * time.Second)
+
+	_, err := http.Get("http://127.0.0.1:8000")
+	if err != nil {
+		t.Errorf("getInfo returns an error %v", err.Error())
 	}
 }
