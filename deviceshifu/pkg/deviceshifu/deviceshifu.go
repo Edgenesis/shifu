67e8ce548998312ddbb60cfb5c810ec9ebfd4fa5
Yongli Chen <yonglichen@edgenesis.com>
2021-09-14 15:33:02 +0000

    http server framework

--- deviceshifu/pkg/deviceshifu/deviceshifu.go
+++ deviceshifu/pkg/deviceshifu/deviceshifu.go
@@ -3,21 +3,47 @@ package deviceshifu
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 type DeviceShifu struct {
-	Name string
+	Name   string
+	server *http.Server
 }
 
-func (ds *DeviceShifu) deviceShifuHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, ds.Name)
+const (
+	DEVICEISHEALTHYSTR string = "Device is healthy"
+)
+
+func New(name string) *DeviceShifu {
+	if name == "" {
+		fmt.Errorf("DeviceShifu's name can't be empty\n")
+		return nil
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", deviceHealthHandler)
+
+	ds := &DeviceShifu{
+		Name: name,
+		server: &http.Server{
+			Addr:         ":8080",
+			Handler:      mux,
+			ReadTimeout:  60 * time.Second,
+			WriteTimeout: 60 * time.Second,
+		},
+	}
+
+	return ds
+}
+
+func deviceHealthHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, DEVICEISHEALTHYSTR)
 }
 
 func (ds *DeviceShifu) startHttpServer(stopCh <-chan struct{}) error {
-	http.HandleFunc("/", ds.deviceShifuHandler)
-	http.ListenAndServe(":8000", nil)
-
-	return nil
+	fmt.Printf("deviceShifu %s's http server started\n", ds.Name)
+	return ds.server.ListenAndServe()
 }
 
 func (ds *DeviceShifu) Start(stopCh <-chan struct{}) error {
