c57cc585c3d24f649796c40e9bb54bf3893ad144
Tom Qin <xiaoyuqin@edgenesis.com>
2021-09-20 06:30:18 +0000

    update script to take arguments

--- test/scripts/deploy-deviceshifu-sample.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-kubectl apply -f deviceshifu/examples/mockdevice/test-sample-edgedevice-serviceaccount.yaml
-kubectl apply -f deviceshifu/examples/mockdevice/test-sample-edgedevice-clusterrole.yaml
-kubectl apply -f deviceshifu/examples/mockdevice/test-sample-edgedevice-crb.yaml
-kubectl apply -f deviceshifu/examples/mockdevice/test-sample-edgedevice-deployment.yaml
-kubectl apply -f deviceshifu/examples/mockdevice/test-sample-edgedevice-service.yaml
---
+++ test/scripts/deviceshifu-sample.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+usage ()
+{
+  echo "usage: $0 apply/delete"
+  exit
+}
+
+if [ "$1" == "apply" ] || [ "$1" == "delete" ]; then
+        kubectl "$1" -f deviceshifu/examples/mockdevice/test-sample-edgedevice-serviceaccount.yaml
+        kubectl "$1" -f deviceshifu/examples/mockdevice/test-sample-edgedevice-clusterrole.yaml
+        kubectl "$1" -f deviceshifu/examples/mockdevice/test-sample-edgedevice-crb.yaml
+        kubectl "$1" -f deviceshifu/examples/mockdevice/test-sample-edgedevice-deployment.yaml
+        kubectl "$1" -f deviceshifu/examples/mockdevice/test-sample-edgedevice-service.yaml
+else
+        echo "not a valid argument, need to be apply/delete"
+        exit 0
+fi
