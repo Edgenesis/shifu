565c8e23d05c0f61bd7dbed856d3e2c301c79ed0
Tom Qin <xiaoyuqin@edgenesis.com>
2021-09-23 04:45:38 +0000

    added deploy and context switching to sample script

--- test/scripts/deviceshifu-sample.sh
+++ test/scripts/deviceshifu-sample.sh
@@ -9,10 +9,11 @@ usage ()
 if [ "$1" == "apply" ] || [ "$1" == "delete" ]; then
         if [ "$1" == "apply" ]; then
                 (cd k8s/crd && make install)
+                (cd k8s/crd && make deploy IMG=edgehub/edgedevice-controller:v0.0.1)
+                kubectl config set-context --current --namespace=crd-system
                 kubectl "$1" -f k8s/crd/config/samples/shifu_v1alpha1_edgedevice.yaml
         else
                 kubectl "$1" -f k8s/crd/config/samples/shifu_v1alpha1_edgedevice.yaml
-                (cd k8s/crd && make uninstall)
         fi
         kubectl "$1" -f deviceshifu/examples/mockdevice/test-edgedevice-mockdevice-serviceaccount.yaml
         kubectl "$1" -f deviceshifu/examples/mockdevice/test-edgedevice-mockdevice-clusterrole.yaml
