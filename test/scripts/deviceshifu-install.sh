7b98deb92c69693e5829f9a7fe1195710ea9d7ba
Tom Qin <xiaoyuqin@edgenesis.com>
2021-10-24 17:50:58 +0800

    update install script

--- test/scripts/deviceshifu-install.sh
+++ test/scripts/deviceshifu-install.sh
@@ -8,11 +8,10 @@ usage ()
 
 if [ "$1" == "apply" ] || [ "$1" == "delete" ]; then
         if [ "$1" == "apply" ]; then
-                (cd k8s/crd && make generate-controller-yaml IMG=edgehub/edgedevice-controller:v0.0.1)
                 kubectl create ns devices
                 kubectl config set-context --current --namespace=default
         fi
-        kubectl "$1" -f k8s/crd
+        kubectl "$1" -f k8s/crd/install
         kubectl "$1" -f deviceshifu/examples/mockdevice/test-edgedevice-mockdevice-serviceaccount.yaml
         kubectl "$1" -f deviceshifu/examples/mockdevice/test-edgedevice-mockdevice-clusterrole.yaml
         kubectl "$1" -f deviceshifu/examples/mockdevice/test-edgedevice-mockdevice-crb.yaml
