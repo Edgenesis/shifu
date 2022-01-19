aabc2a4559b14b0c7d4da5e185ead1f0a6884b20
Tom Qin <xiaoyuqin@edgenesis.com>
2022-01-19 05:12:51 +0000

    add cleanup function to deviceshifu test

--- deviceshifu/pkg/deviceshifu/deviceshifu_config_test.go
+++ deviceshifu/pkg/deviceshifu/deviceshifu_config_test.go
@@ -14,6 +14,7 @@ import (
 const (
 	MOCK_DEVICE_CM_STR               = "configmap_snippet.yaml"
 	MOCK_DEVICE_WRITEFILE_PERMISSION = 0644
+	MOCK_DEVICE_CONFIG_PATH          = "etc"
 )
 
 var MOCK_DEVICE_CONFIG_FOLDER = path.Join("etc", "edgedevice", "config")
