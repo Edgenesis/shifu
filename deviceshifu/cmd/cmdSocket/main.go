90086c4bdd4a2147705dbd827e35dab92fbce53c
Tom Qin <xiaoyuqin@edgenesis.com>
2022-01-20 10:56:34 +0000

    add new cmd, update pipelines yaml

---
+++ deviceshifu/cmd/cmdSocket/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+
+	"github.com/edgenesis/shifu/deviceshifu/pkg/deviceshifuSocket"
+	"k8s.io/apimachinery/pkg/util/wait"
+)
+
+func main() {
+	deviceName := os.Getenv("EDGEDEVICE_NAME")
+	namespace := os.Getenv("EDGEDEVICE_NAMESPACE")
+
+	deviceShifuMetadata := &deviceshifuSocket.DeviceShifuMetaData{
+		deviceName,
+		deviceshifuSocket.DEVICE_CONFIGMAP_FOLDER_PATH,
+		deviceshifuSocket.KUBERNETES_CONFIG_DEFAULT,
+		namespace,
+	}
+
+	ds, err := deviceshifuSocket.New(deviceShifuMetadata)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	if err := ds.Start(wait.NeverStop); err != nil {
+		panic(err.Error())
+	}
+
+	select {}
+}
