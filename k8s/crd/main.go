160ec0c228793460cbd62abe12806fcf7e83da54
xiaoyu0803 <xiaoyu0803>
2022-05-10 14:27:31 +0800

    Unnecessary modifications were removed

--- k8s/crd/main.go
+++ k8s/crd/main.go
@@ -52,14 +52,11 @@ func main() {
 	var metricsAddr string
 	var enableLeaderElection bool
 	var probeAddr string
-	//Get the configurations of metrics bind address,
-	//health probe bind address and leader-elect from the command line
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
-	//Configuration development log
 	opts := zap.Options{
 		Development: true,
 	}
