89ddec6e99cb2368a813f863e83b6aebdd53e4af
Tom Qin <xqin@edgenesis.com>
2022-06-27 03:02:01 +0000

    bumpo up kustomize, controller-gen and rbac-proxy versions

--- test/scripts/deviceshifu-setup.sh
+++ test/scripts/deviceshifu-setup.sh
@@ -11,7 +11,7 @@ if [ "$1" == "apply" ] || [ "$1" == "delete" ]; then
                 (cd /build_dir && for f in *.tar.gz; do docker load < $f; done)
                 kind delete cluster && kind create cluster
                 kind load docker-image nginx:1.21
-                kind load docker-image quay.io/brancz/kube-rbac-proxy:v0.8.0
+                kind load docker-image quay.io/brancz/kube-rbac-proxy:v0.12.0
                 kind load docker-image edgehub/mockdevice-agv:v0.0.1
                 kind load docker-image edgehub/mockdevice-plate-reader:v0.0.1
                 kind load docker-image edgehub/mockdevice-robot-arm:v0.0.1
@@ -24,7 +24,7 @@ if [ "$1" == "apply" ] || [ "$1" == "delete" ]; then
                 docker rmi $(docker images | grep 'edgehub/mockdevice' | awk '{print $3}')
                 docker rmi $(docker images | grep 'edgehub/deviceshifu-http-http' | awk '{print $3}')
                 docker rmi $(docker images | grep 'edgehub/edgedevice-controller' | awk '{print $3}')
-                docker rmi quay.io/brancz/kube-rbac-proxy:v0.8.0
+                docker rmi quay.io/brancz/kube-rbac-proxy:v0.12.0
                 docker rmi $(docker images | grep 'kindest/node' | awk '{print $3}')
                 docker rmi nginx:1.21
         fi
