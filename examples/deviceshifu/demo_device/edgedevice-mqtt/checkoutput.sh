1a1577fc2d78f87643aca3309247545f95f598b0
liyuqi <a964581583@hotmail.com>
2022-09-07 17:34:08 +0800

    fix pipeline

--- examples/deviceshifu/demo_device/edgedevice-mqtt/checkoutput.sh
+++ examples/deviceshifu/demo_device/edgedevice-mqtt/checkoutput.sh
@@ -3,7 +3,7 @@
 default='{"mqtt_message":"","mqtt_receive_timestamp":"0001-01-01 00:00:00 +0000 UTC"}'
 
 
-out=(kubectl exec -it -n deviceshifu nginx -- curl deviceshifu-mqtt/mqtt_data)
+out=shell kubectl exec -it -n deviceshifu nginx -- curl deviceshifu-mqtt/mqtt_data
 
 if [[ $out == $default ]]; then
         echo "equal"
