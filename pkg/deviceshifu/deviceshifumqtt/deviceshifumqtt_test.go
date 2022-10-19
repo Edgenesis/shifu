c72de61c6f152cfcaab66bca696c631923cd3977
Twpeak <2914075814@qq.com>
2022-10-19 13:45:38 +0800

    TestCollectMQTTTelemetry

--- pkg/deviceshifu/deviceshifumqtt/deviceshifumqtt_test.go
+++ pkg/deviceshifu/deviceshifumqtt/deviceshifumqtt_test.go
@@ -1,9 +1,14 @@
 package deviceshifumqtt
 
 import (
+	"errors"
+	"github.com/edgenesis/shifu/pkg/k8s/api/v1alpha1"
+	"github.com/stretchr/testify/assert"
 	"io"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/edgenesis/shifu/pkg/deviceshifu/deviceshifubase"
 	"github.com/edgenesis/shifu/pkg/deviceshifu/unitest"
@@ -81,5 +86,125 @@ func TestDeviceHealthHandler(t *testing.T) {
 
 	if err := mockds.Stop(); err != nil {
 		t.Errorf("unable to stop mock deviceShifu, error: %+v", err)
+	}
+}
+
+func TestCollectMQTTTelemetry(t *testing.T)  {
+	testCases := []struct {
+		Name        string
+		inputDevice *DeviceShifu
+		expErrStr   bool
+		err 		error
+	}{
+		{
+			"case 1 Protocol is nil",
+			&DeviceShifu{
+				base: &deviceshifubase.DeviceShifuBase{
+					Name: "test",
+					EdgeDevice: &v1alpha1.EdgeDevice{
+						Spec: v1alpha1.EdgeDeviceSpec{
+							Address:  unitest.ToPointer("localhost"),
+						},
+
+					},
+				},
+			},
+			false,
+			nil,
+		},
+		{
+			"case 2 Address is nil",
+			&DeviceShifu{
+				base: &deviceshifubase.DeviceShifuBase{
+					Name: "test",
+					EdgeDevice: &v1alpha1.EdgeDevice{
+						Spec: v1alpha1.EdgeDeviceSpec{
+							Protocol:unitest.ToPointer(v1alpha1.ProtocolMQTT),
+						},
+					},
+					DeviceShifuConfig: &deviceshifubase.DeviceShifuConfig{
+						Telemetries: &deviceshifubase.DeviceShifuTelemetries{
+						},
+					},
+					//RestClient: nil,
+				},
+			},
+			false,
+			errors.New("Device test does not have an address"),
+		},
+		{
+			"case 3 DeviceShifuTelemetry Update",
+			&DeviceShifu{
+				base: &deviceshifubase.DeviceShifuBase{
+					Name: "test",
+					EdgeDevice: &v1alpha1.EdgeDevice{
+						ObjectMeta: metav1.ObjectMeta{
+							Namespace: "test_namespace",
+						},
+						Spec: v1alpha1.EdgeDeviceSpec{
+							Address:  unitest.ToPointer("localhost"),
+							Protocol: unitest.ToPointer(v1alpha1.ProtocolMQTT),
+						},
+
+					},
+					DeviceShifuConfig: &deviceshifubase.DeviceShifuConfig{
+						Telemetries: &deviceshifubase.DeviceShifuTelemetries{
+							DeviceShifuTelemetrySettings: &deviceshifubase.DeviceShifuTelemetrySettings{
+								DeviceShifuTelemetryUpdateIntervalInMilliseconds: unitest.ToPointer(time.Since(mqttMessageReceiveTimestamp).Milliseconds() + 1),
+							},
+						},
+					},
+					//RestClient: nil,
+				},
+			},
+			true,
+			nil,
+		},
+		{
+			"case 4 Protocol is http",
+			&DeviceShifu{
+				base: &deviceshifubase.DeviceShifuBase{
+					Name: "test",
+					EdgeDevice: &v1alpha1.EdgeDevice{
+						Spec: v1alpha1.EdgeDeviceSpec{
+							Address:  unitest.ToPointer("localhost"),
+							Protocol: unitest.ToPointer(v1alpha1.ProtocolHTTP),
+						},
+					},
+				},
+			},
+			false,
+			nil,
+		},
+		{
+			"case 5 interval is nil",
+			&DeviceShifu{
+				base: &deviceshifubase.DeviceShifuBase{
+					Name: "test",
+					EdgeDevice: &v1alpha1.EdgeDevice{
+						Spec: v1alpha1.EdgeDeviceSpec{
+							Address:  unitest.ToPointer("localhost"),
+							Protocol: unitest.ToPointer(v1alpha1.ProtocolMQTT),
+						},
+					},
+					DeviceShifuConfig: &deviceshifubase.DeviceShifuConfig{
+						Telemetries: &deviceshifubase.DeviceShifuTelemetries{
+							DeviceShifuTelemetrySettings: &deviceshifubase.DeviceShifuTelemetrySettings{
+							},
+						},
+					},
+				},
+			},
+			false,
+			nil,
+		},
+	}
+
+	for _, c := range testCases {
+		t.Run(c.Name, func(t *testing.T) {
+			got,err :=c.inputDevice.collectMQTTTelemetry()
+			assert.Equal(t, c.expErrStr,got,c.Name)
+			assert.Equalf(t, c.err,err,"error:%s",c.err)
+		})
 	}
 }
