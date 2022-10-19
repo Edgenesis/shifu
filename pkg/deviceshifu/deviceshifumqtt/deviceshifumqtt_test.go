fde74107f71848842fce86b0a33ef94b49f1c515
Twpeak <2914075814@qq.com>
2022-10-19 14:25:54 +0800

    TestCommandHandleMQTTFunc & mock Client and Server

--- pkg/deviceshifu/deviceshifumqtt/deviceshifumqtt_test.go
+++ pkg/deviceshifu/deviceshifumqtt/deviceshifumqtt_test.go
@@ -1,15 +1,26 @@
 package deviceshifumqtt
 
 import (
+	"context"
+	"github.com/edgenesis/shifu/pkg/k8s/api/v1alpha1"
+	"github.com/stretchr/testify/assert"
 	"io"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	"net/http"
+	"net/http/httptest"
 	"os"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/edgenesis/shifu/pkg/deviceshifu/deviceshifubase"
 	"github.com/edgenesis/shifu/pkg/deviceshifu/unitest"
 
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/klog/v2"
+
+	v1 "k8s.io/api/apps/v1"
 )
 
 func TestMain(m *testing.M) {
@@ -82,4 +93,91 @@ func TestDeviceHealthHandler(t *testing.T) {
 	if err := mockds.Stop(); err != nil {
 		t.Errorf("unable to stop mock deviceShifu, error: %+v", err)
 	}
+}
+
+func TestCommandHandleMQTTFunc(t *testing.T) {
+	hs := mockHandlerServer(t)
+	defer hs.Close()
+	addr := strings.Split(hs.URL, "//")[1]
+	mockHandlerHTTP := &DeviceCommandHandlerMQTT{
+		HandlerMetaData: &HandlerMetaData{
+			edgeDeviceSpec: v1alpha1.EdgeDeviceSpec{
+				Address: &addr,
+			},
+		},
+	}
+
+	ds := mockDeviceServer(mockHandlerHTTP, t)
+	defer ds.Close()
+	dc := mockRestClient(ds.URL, "testing")
+
+	// test post method
+	r := dc.Post().Do(context.TODO())
+	assert.Equal(t, "the server rejected our request for an unknown reason", r.Error().Error())
+
+	// test Cannot Encode message to json
+	mqttMessageStr = ""
+	mqttMessageReceiveTimestamp = time.Now()
+	r = dc.Get().Do(context.TODO())
+	assert.Nil(t, r.Error())
+}
+
+func mockRestClient(host string, path string) *rest.RESTClient {
+	c, err := rest.RESTClientFor(
+		&rest.Config{
+			Host:    host,
+			APIPath: path,
+			ContentConfig: rest.ContentConfig{
+				GroupVersion:         &v1.SchemeGroupVersion,
+				NegotiatedSerializer: scheme.Codecs.WithoutConversion(),
+			},
+		},
+	)
+	if err != nil {
+		klog.Errorf("mock client for host %s, apipath: %s failed,", host, path)
+		return nil
+	}
+
+	return c
+}
+
+type MockCommandHandler interface {
+	commandHandleFunc() http.HandlerFunc
+}
+
+func mockDeviceServer(h MockCommandHandler, t *testing.T) *httptest.Server {
+	// catch device http request and response properly with specific paths
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		switch path {
+		case "/testing/apps/v1":
+			klog.Info("ds get testing call, calling the handler server")
+			assert.Equal(t, "/testing/apps/v1", path)
+			f := h.commandHandleFunc()
+			f.ServeHTTP(w, r)
+		default:
+			w.Header().Add("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			klog.Info("ds default request, path:", path)
+		}
+	}))
+	return server
+}
+
+func mockHandlerServer(t *testing.T) *httptest.Server {
+	// catch handler http request and response properly with specific paths
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		switch path {
+		case "/test_instruction":
+			w.Header().Add("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			klog.Info("handler get the instruction and executed.")
+		default:
+			w.WriteHeader(http.StatusOK)
+			klog.Info("hs get default request, path:", path)
+		}
+
+	}))
+	return server
 }
