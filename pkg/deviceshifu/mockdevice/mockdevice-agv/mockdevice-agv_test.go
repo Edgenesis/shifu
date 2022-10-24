4f93e4d6f6a51b1864aa5c9a929fbfff267ca04e
Twpeak <2914075814@qq.com>
2022-10-24 19:16:59 +0800

    repush

--- pkg/deviceshifu/mockdevice/mockdevice-agv/mockdevice-agv_test.go
+++ pkg/deviceshifu/mockdevice/mockdevice-agv/mockdevice-agv_test.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 	"testing"
 	"time"
 )
@@ -18,34 +19,54 @@ func TestInstructionHandler(t *testing.T) {
 	os.Setenv("MOCKDEVICE_NAME", "mockdevice_test")
 	os.Setenv("MOCKDEVICE_PORT", "12345")
 	mocks := []struct {
-		name string
-		url  string
+		name       string
+		url        string
+		StatusCode int
+		expResult  interface{}
 	}{
 		{
-			"case 1 prot 12345 get_status",
+			"case 1 port 12345 get_status",
 			"http://localhost:12345/get_status",
+			200,
+			[]string{"Running", "Idle", "Busy", "Error"},
 		},
 		{
-			"case 2 prot 12345 get_position",
+			"case 2 port 12345 get_position",
 			"http://localhost:12345/get_position",
+			200,
+			true,
 		},
 	}
 
 	go mockdevice.StartMockDevice(availableFuncs, instructionHandler)
 
-	time.Sleep(1 * time.Second)
+	time.Sleep(100 * time.Microsecond)
 
 	for _, c := range mocks {
 		t.Run(c.name, func(t *testing.T) {
 			resp, err := http.Get(c.url)
-			if err != nil {
-				t.Fatalf("HTTP GET returns an error %v", err.Error())
-			}
-			defer resp.Body.Close()
 			assert.Nil(t, err)
-
+			defer resp.Body.Close()
 			body, _ := io.ReadAll(resp.Body)
-			assert.NotNil(t, body)
+
+			switch {
+			case strings.Contains(c.url, "/get_position"):
+				assert.Equal(t, c.expResult, check(string(body)))
+			case strings.Contains(c.url, "/get_status"):
+				assert.Contains(t, c.expResult, string(body))
+			}
 		})
 	}
+}
+
+func check(Result string) bool {
+	res := true
+	expResult := []string{"xpos", "ypos"}
+	for _, v := range expResult {
+		if !strings.Contains(Result, v) {
+			res = false
+			break
+		}
+	}
+	return res
 }
