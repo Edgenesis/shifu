19bf1ae09c03583d4480ca81ae4324b734dd7622
Yongli Chen <yonglichen@edgenesis.com>
2022-10-27 07:34:09 +0000

    add test template

---
+++ pkg/deviceshifu/deviceshifutemplate/deviceshifutemplate_test.go
@@ -0,0 +1,12 @@
+// Barebone deviceshifu test code. You need to modify this accordingly.
+package deviceshifutemplate
+
+import (
+	"testing"
+)
+
+func TestNew(t *testing.T) {}
+
+func TestStart(t *testing.T) {}
+
+func TestStop(t *testing.T) {}
