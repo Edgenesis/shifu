8e6038f68bb3b6b12bebda61f532770f37f68edc
cbgz121 <58777451+cbgz121@users.noreply.github.com>
2022-12-15 08:42:52 +0800

    mqtt multi-topic (#407)
    * mqtt multi-topic
    
    * mqtt multi-topic
    
    * mqtt multi-topic
    
    * mqttconfig_test update
    
    * multi-topic
    
    * mqtt multi-topic
    
    * mqtt multi-topic
    
    * mqtt multi-topic
    
    * mqtt multi-topic
    
    * mqtt multi-topic
    
    * mqtt multi-topic
    
    * mqtt multi-topic
    
    * mqtt multi-topic
    
    * mqtt multi-topic
    
    * mqtt multi-topic
    
    * mqtt multi-topic
    
    * mqtt multi-topic
    
    * mqtt multi-topic
    
    * mqtt multi-topic
    
    * mqtt multi-topic
    
    * mqtt multi-topic
    
    * mqtt multi-topic
    
    * mqtt multi-topic
    
    * mqtt multi-topic
    
    * mqtt multi-topic
    
    * mqtt multi-topic

--- pkg/deviceshifu/deviceshifumqtt/deviceshifumqttconfig.go
+++ pkg/deviceshifu/deviceshifumqtt/deviceshifumqttconfig.go
@@ -1,5 +1,15 @@
 package deviceshifumqtt
 
+
+import (
+	"k8s.io/klog/v2"
+	"github.com/edgenesis/shifu/pkg/deviceshifu/deviceshifubase"
+)
+
+const (
+	mqttTopic = "MQTTTopic"
+)
+
 // ReturnBody Body of mqtt's reply
 type ReturnBody struct {
 	MQTTMessage   string `json:"mqtt_message"`
@@ -8,3 +18,37 @@ type ReturnBody struct {
 
 // RequestBody Body of mqtt's request by POST method
 type RequestBody string
+
+
+// MQTTInstructions MQTT Instructions
+type MQTTInstructions struct {
+	Instructions map[string]*MQTTInstruction
+}
+
+// MQTTInstruction MQTT Instruction
+type MQTTInstruction struct {
+	MQTTProtocolProperty *MQTTProtocolProperty 
+}
+
+// MQTTProtocolProperty MQTT Instruction's Property
+type MQTTProtocolProperty struct {
+	MQTTTopic string
+}
+
+// CreateMQTTInstructions Create MQTT Instructions
+func CreateMQTTInstructions(dsInstructions *deviceshifubase.DeviceShifuInstructions) *MQTTInstructions {
+	instructions := make(map[string]*MQTTInstruction)
+
+	for key, dsInstruction := range dsInstructions.Instructions {
+		if dsInstruction.DeviceShifuProtocolProperties != nil && dsInstruction.DeviceShifuProtocolProperties[mqttTopic] == "" {
+			klog.Fatalf("Error when Read MQTTTopic From DeviceShifuInstructions, error: instruction %v has an empty topic", key)
+		}
+		instruction := &MQTTInstruction{
+			&MQTTProtocolProperty{
+				MQTTTopic: dsInstruction.DeviceShifuProtocolProperties[mqttTopic],
+			},
+		}
+		instructions[key] = instruction
+	}
+	return &MQTTInstructions{instructions}
+}
