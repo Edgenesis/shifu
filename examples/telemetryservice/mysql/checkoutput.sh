493df4d795a9c905d2ed6cfba22833c3ab963c60
Yunxe <784312513@qq.com>
2023-08-16 15:38:59 +0800

    wip:e2e test

--- examples/telemetryservice/mysql/checkoutput.sh
+++ examples/telemetryservice/mysql/checkoutput.sh
@@ -1,24 +1,34 @@
-#!bin/bash
+#!/bin/bash
 MySQLOutput=2
-for i in {1..30}
+sleep 2
+for i in {1..50}
 do
-    output=$(docker exec mysql mysql -h localhost -u root -p"" -e "Show databases;" | grep 'error' | wc -l)
-    echo $output
+    output=$(docker exec mysql mysql -u root -e "Show databases;" 2>&1 | grep 'ERROR' | wc -l)
+    #echo $output
     if [[ $output -eq 0 ]]
     then
+        echo $output
         break
-    elif [[ $i -eq 30 ]]
+    elif [[ $i -eq 50 ]]
     then
         exit 1
     fi
 done
+
 # init MySQL Table
-docker exec mysql mysql -h localhost -u root -p"" < /root/init.sql
+
+docker exec mysql mysql -u root \
+    -e "Create database shifu;
+        Use shifu;
+        CREATE TABLE testTable ( TelemetryID INT AUTO_INCREMENT PRIMARY KEY, DeviceName VARCHAR(255), TelemetryData TEXT, TelemetryTimeStamp DATETIME );
+        Select * From testTable;" 
+
+
 
 for i in {1..30}
 do
-    docker exec nginx curl localhost:9090/sql/mysql
-    output=$(docker exec mysql mysql -h localhost -u root -p"" -e "Use shifu;" -e "Select TelemetryData from testTable where TelemetryData='testData' limit 10;" | grep 'testData' | wc -l)
+    docker exec nginx curl 127.0.0.1:9090/sql/mysql
+    output=$(docker exec mysql mysql -u root -e "Use shifu;Select TelemetryData from testTable where TelemetryData='testData' limit 10;" | grep 'testData' | wc -l)
     echo $output
     if [[ $output -ge $MySQLOutput ]]
     then
--- examples/telemetryservice/mysql/init.sql
+++ examples/telemetryservice/mysql/init.sql
@@ -1,4 +1,4 @@
 Create database shifu;
 Use shifu;
-CREATE TABLE testTable ( TelemetryID INT IDENTITY(1,1) PRIMARY KEY, DeviceName VARCHAR(255), TelemetryData TEXT, TelemetryTimeStamp DATETIME );
+CREATE TABLE testTable ( TelemetryID INT AUTO_INCREMENT PRIMARY KEY, DeviceName VARCHAR(255), TelemetryData TEXT, TelemetryTimeStamp DATETIME );
 Select * From testTable;
