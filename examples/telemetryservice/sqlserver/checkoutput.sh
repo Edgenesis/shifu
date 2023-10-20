717da84464690aee071eedaf9c4379b58fd81978
yunxe <784312513@qq.com>
2023-10-20 12:44:25 +0800

    modified users

--- examples/telemetryservice/sqlserver/checkoutput.sh
+++ examples/telemetryservice/sqlserver/checkoutput.sh
@@ -17,13 +17,13 @@ do
 done
 
 # init SQLServer Table
-docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P Some_Strong_Password \
+docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Some_Strong_Password \
     -Q "Create database shifu;"
 
-docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P Some_Strong_Password \
+docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Some_Strong_Password \
     -d shifu -Q "CREATE TABLE testTable ( TelemetryID INT IDENTITY(1,1) PRIMARY KEY, DeviceName VARCHAR(255), TelemetryData TEXT, TelemetryTimeStamp DATETIME );"
 
-docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P Some_Strong_Password \
+docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Some_Strong_Password \
     -d shifu -Q "Select * from shifu.dbo.testTable;"
 
 for i in {1..30}
