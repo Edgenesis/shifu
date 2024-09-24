07964603630254c5915aa55637a929bc7e4cbcd9
Tom Qin <xqin@edgenesis.com>
2024-09-24 21:26:23 +0900

    update sql server image to latest (#983)
    * update sql server image to latest
    
    * update sql command
    
    * update sql check command in test script
    
    * update mock password in test client

--- examples/telemetryservice/sqlserver/checkoutput.sh
+++ examples/telemetryservice/sqlserver/checkoutput.sh
@@ -7,20 +7,20 @@ done
 echo "SQL Server is ready."
 
 # init SQLServer Table
-docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Some_Strong_Password \
+docker exec sqlserver /opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -C -P YourStrong@Passw0rd \
     -Q "Create database shifu;"
 
-docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Some_Strong_Password \
+docker exec sqlserver /opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -C -P YourStrong@Passw0rd \
     -d shifu -Q "CREATE TABLE testTable ( TelemetryID INT IDENTITY(1,1) PRIMARY KEY, DeviceName VARCHAR(255), TelemetryData TEXT, TelemetryTimeStamp DATETIME );"
 
-docker exec sqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Some_Strong_Password \
+docker exec sqlserver /opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -C -P YourStrong@Passw0rd \
     -d shifu -Q "Select * from shifu.dbo.testTable;"
 
 for i in {1..30}
 do
     docker exec nginx curl localhost:9090/sqlserver
-    output=$(docker exec sqlserver /opt/mssql-tools/bin/sqlcmd  \
-    -S localhost -U sa -P Some_Strong_Password \
+    output=$(docker exec sqlserver /opt/mssql-tools18/bin/sqlcmd  \
+    -S localhost -U sa -C -P YourStrong@Passw0rd \
     -Q "SELECT TOP 10 TelemetryData FROM shifu.dbo.testTable WHERE CAST(TelemetryData AS VARCHAR(MAX)) = 'testData'" | grep 'testData' | wc -l)
     echo $output
     if [[ $output -ge $SQLServerOutput ]]
