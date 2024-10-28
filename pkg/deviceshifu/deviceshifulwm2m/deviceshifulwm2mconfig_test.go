492dba3d5f8d6fa39c4240ba889ee99432893343
yq <1747160227@qq.com>
2024-10-28 17:53:01 +0800

    modify test code

--- pkg/deviceshifu/deviceshifulwm2m/deviceshifulwm2mconfig_test.go
+++ pkg/deviceshifu/deviceshifulwm2m/deviceshifulwm2mconfig_test.go
@@ -4,86 +4,47 @@ import (
 	"testing"
 
 	"github.com/edgenesis/shifu/pkg/deviceshifu/deviceshifubase"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCreateLwM2MInstructions(t *testing.T) {
 	tests := []struct {
-		name          string
-		input         *deviceshifubase.DeviceShifuInstructions
-		expectedError bool
-		expected      *LwM2MInstruction
+		name           string
+		input          *deviceshifubase.DeviceShifuInstructions
+		expected       *LwM2MInstruction
+		expectingPanic bool
 	}{
 		{
-			name: "Valid instructions",
+			name: "Valid Instructions",
 			input: &deviceshifubase.DeviceShifuInstructions{
 				Instructions: map[string]*deviceshifubase.DeviceShifuInstruction{
 					"instruction1": {
 						DeviceShifuProtocolProperties: map[string]string{
-							objectIdStr:      "123",
+							objectIdStr:      "1",
 							enableObserveStr: "true",
 						},
 					},
 				},
 			},
-			expectedError: false,
 			expected: &LwM2MInstruction{
 				Instructions: map[string]*LwM2MProtocolProperty{
 					"instruction1": {
-						ObjectId:      "123",
+						ObjectId:      "1",
 						EnableObserve: true,
 					},
 				},
 			},
-		},
-		{
-			name: "Missing ObjectId",
-			input: &deviceshifubase.DeviceShifuInstructions{
-				Instructions: map[string]*deviceshifubase.DeviceShifuInstruction{
-					"instruction1": {
-						DeviceShifuProtocolProperties: map[string]string{
-							enableObserveStr: "true",
-						},
-					},
-				},
-			},
-			expectedError: true,
-		},
-		{
-			name: "Missing EnableObserve",
-			input: &deviceshifubase.DeviceShifuInstructions{
-				Instructions: map[string]*deviceshifubase.DeviceShifuInstruction{
-					"instruction1": {
-						DeviceShifuProtocolProperties: map[string]string{
-							objectIdStr: "123",
-						},
-					},
-				},
-			},
-			expectedError: true,
+			expectingPanic: false,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			defer func() {
-				if r := recover(); r != nil {
-					if !tt.expectedError {
-						t.Errorf("unexpected error: %v", r)
-					}
-				}
-			}()
-
-			result := CreateLwM2MInstructions(tt.input)
-			if !tt.expectedError && result != nil {
-				for key, prop := range result.Instructions {
-					expectedProp, exists := tt.expected.Instructions[key]
-					if !exists {
-						t.Errorf("unexpected instruction key: %v", key)
-					}
-					if prop.ObjectId != expectedProp.ObjectId || prop.EnableObserve != expectedProp.EnableObserve {
-						t.Errorf("unexpected instruction properties for key %v: got %v, want %v", key, prop, expectedProp)
-					}
-				}
+			if tt.expectingPanic {
+				assert.Panics(t, func() { CreateLwM2MInstructions(tt.input) })
+			} else {
+				result := CreateLwM2MInstructions(tt.input)
+				assert.Equal(t, tt.expected, result)
 			}
 		})
 	}
