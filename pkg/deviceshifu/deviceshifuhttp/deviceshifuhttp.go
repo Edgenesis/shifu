fbf788bb1389516a5b329732edf09c06a3beb091
Tom Qin <xqin@edgenesis.com>
2025-02-03 17:10:04 +0800

    [Issue #1122] Add support for HTTP PUT request in deviceShifu HTTP (#1124)
    * [Issue #1122] Add support for HTTP PUT request in deviceShifu HTTP
    
    * add response body close to prevent resource leak

--- pkg/deviceshifu/deviceshifuhttp/deviceshifuhttp.go
+++ pkg/deviceshifu/deviceshifuhttp/deviceshifuhttp.go
@@ -181,7 +181,7 @@ func (handler DeviceCommandHandlerHTTP) commandHandleFunc() http.HandlerFunc {
 		}
 
 		switch reqType {
-		case http.MethodPost:
+		case http.MethodPost, http.MethodPut:
 			requestBody, parseErr = io.ReadAll(r.Body)
 			if parseErr != nil {
 				http.Error(w, "Error on parsing body", http.StatusBadRequest)
@@ -222,6 +222,7 @@ func (handler DeviceCommandHandlerHTTP) commandHandleFunc() http.HandlerFunc {
 		}
 
 		if resp != nil {
+			defer resp.Body.Close()
 			// Handling deviceshifu stuck when responseBody is a stream
 			instructionFuncName, shouldUsePythonCustomProcessing := deviceshifubase.CustomInstructionsPython[handlerInstruction]
 			if !shouldUsePythonCustomProcessing {
@@ -385,6 +386,7 @@ func (handler DeviceCommandHandlerHTTPCommandline) commandHandleFunc() http.Hand
 		}
 
 		if resp != nil {
+			defer resp.Body.Close()
 			utils.CopyHeader(w.Header(), resp.Header)
 			w.WriteHeader(resp.StatusCode)
 
@@ -463,6 +465,7 @@ func (ds *DeviceShifuHTTP) collectHTTPTelemtries() (bool, error) {
 				}
 
 				if resp != nil {
+					defer resp.Body.Close()
 					if resp.StatusCode >= http.StatusOK && resp.StatusCode < http.StatusMultipleChoices {
 						instructionFuncName, pythonCustomExist := deviceshifubase.CustomInstructionsPython[instruction]
 						if pythonCustomExist {
